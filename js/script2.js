e0a7d31df0cd3ac19555744f3a8da682404ad762
Jennifer Mah <jema5807@colorado.edu>
2019-02-09 16:16:29 -0700

    changed files

--- js/script2.js
+++ js/script2.js
@@ -1,43 +1,127 @@
-// Get a reference to the database service
-var database = firebase.database();
+// // NO AUTHENTICATE
 
-// Create a storage reference from our storage service
-var storageRef = database.ref("orders");
+// // Get a reference to the database service
+// var database = firebase.database();
 
-//create instance of the Google provider instance
-var provider = new firebase.auth.GoogleAuthProvider();
+// // Create a storage reference from our storage service
+// var storageRef = database.ref("orders");
 
-//user status div
-var user_info = document.getElementById("user_status");
+// //create instance of the Google provider instance
+// var provider = new firebase.auth.GoogleAuthProvider();
 
-//current user
-var current_user = null;
+// //user status div
+// var user_info = document.getElementById("user_status");
 
-function authenticate(){
+// //current user
+// var current_user = null;
+
+// function order()
+// {
+// 		var name;
+// 		var email;
+// 		var street;
+// 		var city;
+// 		var state;
+// 		var zip;
+// 		var country;
+// 		var pickup;
+// 		var pay_type; 
+// 		var card_num;
+// 		var exp_date;
+// 		var cookies;
+// 		var macaroons;
+// 		var choclate;
+
+// 		if (document.getElementById("cookies").checked == true){
+// 			//cookies
+// 			cookies = document.getElementById("cookies").value;
+// 		}
+// 		else {
+// 			cookies = "none";
+// 		}
+// 		if (document.getElementById("macaroons").checked == true){
+// 			//macaroons
+// 			macaroons = document.getElementById("macaroons").value;
+// 		}
+// 		else {
+// 			macaroons= "none";
+// 		}
+// 		if (document.getElementById("CC").checked == true){
+// 			//choclate
+// 			choclate = document.getElementById("CC").value;
+// 		}
+// 		else {
+// 			choclate= "none";
+// 		}
+		
+// 		//team
+// 		pickup = document.getElementById("location").value;
 	
-}
-
-function signOut(){
+// 		//name
+// 		name = document.getElementById("name").value;
+		
+// 		//email
+// 		email = document.getElementById("email").value;
 	
-}
+// 		//address
+// 		street = document.getElementById("address").value;
+		
+// 		//city
+// 		city = document.getElementById("city").value;
+		
+// 		//state
+// 		state = document.getElementById("state").value;
+		
+// 		//zip
+// 		zip = document.getElementById("zip").value;
+		
+// 		//country
+// 		country = document.getElementById("country").value;
+			
+// 		//payment
+// 		if (document.getElementById("paypal").checked == true){
+// 			pay_type = document.getElementById("paypal").value;
+// 		} else {
+// 			if (document.getElementById("visa").checked == true){
+// 				pay_type = document.getElementById("visa").value;
+// 			} else {
+// 				if (document.getElementById("mastercard").checked == true){
+// 					pay_type = document.getElementById("mastercard").value;
+// 				} else {
+// 					pay_type = "none";
+// 				}
+// 			}
+// 		}
+		
+// 		//card number
+// 		card_num = document.getElementById("cardNumber").value;
+		
+// 		//expiration date
+// 		exp_date = document.getElementById("expiration").value;
+		
+// 		//write to firebase
+// 		storageRef.push({
+// 			name: name, 
+// 			billing_address:{
+// 				address: street,
+// 				city: city,
+// 				state: state,
+// 				zip: zip,
+// 				country: country
+// 			},
+// 			email: email,
+// 			cookie: cookies,
+// 			macaroons: macaroons,
+// 			Pain_au_chocolat: choclate, 
+// 			location: pickup, 
+// 			payment: {
+// 				type: pay_type,
+// 				card: card_num,
+// 				exp: exp_date
+// 			}
+// 			}).then(window.location.href = "firebase_order_confirm.html");
+		
+		
+// }
 
-//setting an event listener for change of authentication state
-firebase.auth().onAuthStateChanged(function(user) {
-	current_user=user;
-	if (user) {
-    	// User is signed in
-		user_info.innerHTML = "Hi, " + user.displayName;
-  	} else {
-    	// No user is signed in
-		user_info.innerHTML = "";
-  	}
-});
-
-function order()
-{
-	
-}
-
-document.getElementById("enter").addEventListener("click", order);
-document.getElementById("signin").addEventListener("click", authenticate);
-document.getElementById("signout").addEventListener("click", signOut);
+// document.getElementById("enter").addEventListener("click", order);
